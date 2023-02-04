9b13d9528b01fd1aac5b1748108b685aa7b1b43f
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2023-02-04 15:43:18 +0100

    refactor: ♻️ basic tests

--- src/tests/2-basic/basic.test.ts
+++ src/tests/2-basic/basic.test.ts
@@ -2,17 +2,31 @@ import {containsDuplicate, hasExactString} from './basic';
 
 const shoppingList = ['diapers', 'kleenex', 'trash bags', 'paper towels', 'beer'];
 
-describe('the shopping list', () => {
-  it('should have a beer on it', () => {
-    expect(shoppingList).toContain('beer');
+describe('Shopping list', () => {
+  describe('when the shopping list is empty', () => {
+    const emptyShoppingList: string[] = [];
+
+    it('should be empty', () => {
+      expect(emptyShoppingList).toHaveLength(0);
+    });
   });
 
-  it(`should not have a cocoa on it`, () => {
-    expect(shoppingList).not.toContain('cocoa');
+  describe('when the shopping list is not empty', () => {
+    it('should have a beer on it', () => {
+      expect(shoppingList).toContain('beer');
+    });
+
+    it(`should not have a cocoa on it`, () => {
+      expect(shoppingList).not.toContain('cocoa');
+    });
   });
 });
 
 describe('hasExactString', () => {
+  it('should return false when the list is empty', () => {
+    expect(hasExactString([], 'beer')).toBe(false);
+  });
+
   describe('should return true when list contains', () => {
     it('beer', () => {
       expect(hasExactString(shoppingList, 'beer')).toBe(true);
@@ -31,10 +45,6 @@ describe('hasExactString', () => {
     it('milk', () => {
       expect(hasExactString(shoppingList, 'milk')).toBe(false);
     });
-  });
-
-  it('should return false when the list is empty', () => {
-    expect(hasExactString([], 'beer')).toBe(false);
   });
 });
 
